2a79fad88ba8a66ce467ee059e2942633af67fbe
Luka Matic <maticluka999@gmail.com>
2023-05-23 17:59:05 +0200

    fix: fixed tests

--- test/pipeline.spec.ts
+++ test/pipeline.spec.ts
@@ -3,7 +3,7 @@ const { gzip } = require('../examples/example.js')
 describe('simple handler', () => {
   it('awaits and returns the response', async () => {
     const resp = await gzip('hello')
-    const parsed = JSON.parse(resp)
+    const parsed = JSON.parse(resp.body)
     expect(parsed).toMatchObject({ hello: 'world' })
   })
 })

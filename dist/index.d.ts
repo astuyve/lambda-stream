08f449f60b1b49c8cfb22c61f73957000a9a50b1
Simon Garner <simon@equalogic.com>
2024-10-10 12:37:14 +1300

    update dist

--- dist/index.d.ts
+++ dist/index.d.ts
@@ -1,7 +1,8 @@
 import { APIGatewayProxyEventV2, Callback, Context } from 'aws-lambda';
 import { ResponseStream } from './ResponseStream';
 export declare function isInAWS(): boolean;
-export type RequestHandler<Event extends Record<string, any> = APIGatewayProxyEventV2> = (ev: Event, streamResponse: ResponseStream, ctx?: Context, callback?: Callback) => any | Promise<any>;
-export declare function streamifyResponse<Event extends Record<string, any>>(handler: RequestHandler<Event>): RequestHandler<Event>;
+export type StreamingHandler<TEvent = APIGatewayProxyEventV2, TResult = any> = (ev: TEvent, streamResponse: ResponseStream, ctx?: Context, callback?: Callback<TResult>) => void | Promise<TResult>;
+export type RequestHandler<TResult = any> = StreamingHandler<APIGatewayProxyEventV2, TResult>;
+export declare function streamifyResponse<TEvent = any, TResult = any>(handler: StreamingHandler<TEvent, TResult>): StreamingHandler<TEvent, TResult>;
 export { ResponseStream } from './ResponseStream';
 //# sourceMappingURL=index.d.ts.map
